0d07f52403aa6e568c1a52d38a0c97bd08eb362f
Jackson Silva <jackson.silva.who@gmail.com>
2025-03-24 20:12:53 -0300

    adding poster-card component

---
+++ src/app/components/poster-card/poster-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-poster-card',
+  templateUrl: './poster-card.component.html',
+  styleUrls: ['./poster-card.component.css']
+})
+export class PosterCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
