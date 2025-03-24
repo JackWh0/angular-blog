3db2bd3070d2500fcc145c73e4ff152e53fd4a14
Jackson Silva <jackson.silva.who@gmail.com>
2025-03-24 13:49:57 -0300

    default component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { PosterCardComponent } from './components/poster-card/poster-card.component';
+import { InfoCardComponent } from './components/info-card/info-card.component';
+import { EpisodeCardComponent } from './components/episode-card/episode-card.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    PosterCardComponent,
+    InfoCardComponent,
+    EpisodeCardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
