5545e2eac3c63714b4e2c89e0c8d2eba1a1eef83
Jackson Silva <jackson.silva.who@gmail.com>
2025-04-16 17:31:35 -0300

    setting routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ReviewsComponent } from './pages/reviews/reviews.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'reviews',
+    component: ReviewsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
